2f65140e0cebb15dc880b993952659385292be80
visionarycoder7 <143948847+visionarycoder7@users.noreply.github.com>
2026-09-21 23:59:36 +0530

    Add files via upload

---
+++ fstring.py
@@ -0,0 +1,16 @@
+"""name = "SOHAN"
+age = 21
+language = "Python"
+hours = 3
+'''print(name, "is the name and is ",  age, "years old")
+'''
+print(name, "is the name and is ",  age, "years old.He studies", language , hours, "hours a day. ")
+
+#using fstring
+print(f"{name} is {age} years old.He studies {language}  {hours} hours a day.")"""
+sub1 = 89
+sub2 = 78
+sub3 = 89
+total = sub1 + sub2 + sub3
+name = "sohan"
+print(f"{name} got {total} marks in total and {(total/300)*100} ")
