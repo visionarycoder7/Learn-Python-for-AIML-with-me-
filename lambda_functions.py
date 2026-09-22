c20958c813f8a3b1c051b1f3655181c77c4f5aa5
visionarycoder7 <143948847+visionarycoder7@users.noreply.github.com>
2026-09-22 23:43:46 +0530

    Add files via upload

---
+++ lambda_functions.py
@@ -0,0 +1,14 @@
+add = lambda x,y:x+y
+print(add(8,7))
+
+sub = lambda x,y:x-y
+print(sub(234164,23131))
+
+mul = lambda x,y:x*y
+print(mul(9999,1))
+
+square = lambda x:x*x
+print(square(4))
+
+cube = lambda x:x**3
+print(cube(4))
